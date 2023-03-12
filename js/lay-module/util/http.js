a923ba7e3bc281347b0b6316c64c3b419d446914
zhangqinghua <the.patron.saint.of.science@gmail.com>
2023-03-12 17:54:40 +0800

    做了一系列修改。

--- js/lay-module/util/http.js
+++ js/lay-module/util/http.js
@@ -2,8 +2,8 @@ layui.define(['layer'], function (exports) {
     "use strict";
 
     let dateUtils = {
-         // api:"http://167.179.79.96:8082"  //根据自己项目的端口而定
-         api:"http://127.0.0.1:8082"  //根据自己项目的端口而定
+         api:"http://167.179.79.96:8082"  //根据自己项目的端口而定
+         // api:"http://127.0.0.1:8082"  //根据自己项目的端口而定
     }
 
     exports("http", dateUtils);
