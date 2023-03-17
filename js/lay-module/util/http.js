183c22dbc327d6890208ae858496e7be7dcf0d1d
zhangqinghua <the.patron.saint.of.science@gmail.com>
2023-03-17 19:04:15 +0800

    添加搜索条件

--- js/lay-module/util/http.js
+++ js/lay-module/util/http.js
@@ -3,7 +3,7 @@ layui.define(['layer'], function (exports) {
 
     let dateUtils = {
          api:"http://167.179.79.96:8082"  //根据自己项目的端口而定
-          // api:"http://127.0.0.1:8082"  //根据自己项目的端口而定
+         // api:"http://127.0.0.1:8082"  //根据自己项目的端口而定
     }
 
     exports("http", dateUtils);
